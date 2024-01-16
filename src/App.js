c0602966ed651619fc28d606d36e1b0251efb7f1
Chris Banning <chrisbanning510@gmail.com>
2024-01-16 01:29:01 -0500

    comment out auto backend update upon version selection

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ const App = () => {
       setCurrentForm(selectedForm);
       setCurrentVersion(selectedForm.version);
     }
-    backendUpdateFunction();
+    // backendUpdateFunction();
   };
 
   // this is working great!!!!!!!!!!!!!!!!
